b39676c0448701adf65506b32b309d3e0e758b84
gajendra <davegk59@gmil.com>
2024-05-14 10:13:37 +0530

    this is static complited

--- shop/urls.py
+++ shop/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('add-to-cart/',views.add_to_cart,name='add-to-cart/'),
     path('buy-now/',views.buy_now,name='buy-now'),
     path('address/',views.address,name='address'),
+    path('orders/',views.orders,name='orders'),
+    path('change-password/',views.change_password,name='change-password'),
+    path('mobile/',views.mobile,name='mobile'),
+    path('login/',views.login,name='login'),
+    path('registration/',views.customerregistraion,name='registration'),
+    path('checkout/',views.checkout,name='checkout'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -14,3 +14,15 @@ def buy_now(request):
     return render(request,'shop/buynow.html')
 def address(request):
     return render(request,'shop/address.html')
+def orders(request):
+    return render(request,'shop/orders.html')
+def change_password(request):
+    return render(request,'shop/changepassword.html')
+def mobile(request):
+    return render(request,'shop/mobile.html')
+def login(request):
+    return render(request,'shop/login.html')
+def customerregistraion(request):
+    return render(request,'shop/customerregistration.html')
+def checkout(request):
+    return render(request,'shop/checkout.html')
