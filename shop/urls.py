67b3730b83289ef9f12754410f4d6fc916b34650
gajendra <davegk59@gmil.com>
2024-05-14 19:30:21 +0530

    added some features

--- shop/urls.py
+++ shop/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.home),
     path('product-detail/',views.product_detail,name='product-detail'),
-    path('add-to-cart/',views.add_to_cart,name='add-to-cart/'),
+    path('add-to-cart/',views.add_to_cart,name='add-to-cart'),
     path('buy-now/',views.buy_now,name='buy-now'),
     path('address/',views.address,name='address'),
     path('orders/',views.orders,name='orders'),
