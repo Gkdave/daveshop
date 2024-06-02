48b490107d2caca786d3488979b6d20c6114430c
gajendra <davegk59@gmil.com>
2024-06-02 17:53:48 +0530

    change in view

--- shop/views.py
+++ shop/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import redirect, render
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 from django.views import View
 from django.views import generic 
 from .models import Product,Customer,Cart,OrderPlaced
 from .forms import CustomerProfileForm, CustomerRegistrationForm 
-=======
+
 from django.views import View 
 from .models import Product,Customer,Cart,OrderPlaced
 from .form import CustomerProfileForm, CustomerRegistrationForm 
->>>>>>> 238eb6a2b9b0d5a1832138bb78a1e41d38c2131c
-=======
+
 from django.views import View 
 from .models import Product,Customer,Cart,OrderPlaced
 from .form import CustomerProfileForm, CustomerRegistrationForm 
->>>>>>> 238eb6a2b9b0d5a1832138bb78a1e41d38c2131c
+
 from django.contrib import messages 
 from django.db.models import Q 
 from django.http import JsonResponse 
@@ -185,22 +184,22 @@ def bottomwear(request,data=None):
 
 def login(request):
     return render(request,'shop/login.html')
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 # def logout(request):
 #     return redirect('/accounts/login/')
 class LogOut(generic.View):
     def get(self,request):
         LogOut()
         return redirect('/accounts/login/')
-=======
+
 def logout(request):
     return redirect('/accounts/login/')
->>>>>>> 238eb6a2b9b0d5a1832138bb78a1e41d38c2131c
-=======
+
+
 def logout(request):
     return redirect('/accounts/login/')
->>>>>>> 238eb6a2b9b0d5a1832138bb78a1e41d38c2131c
+
 class CustomerRegistrationView(View):
     def get(self,request):
         form = CustomerRegistrationForm
